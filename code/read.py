3afb9ad6863a82a806653c04255d1e07542b6981
spacebody <510662916@qq.com>
2018-02-09 21:49:14 +0800

    Merge

--- code/read.py
+++ code/read.py
@@ -3,26 +3,37 @@
 import pandas as pd 
 import re 
 import numpy as np
+import os
+import sys
 
-seseds = pd.read_csv('C:\\Users\\THINKPAD\\PycharmProjects\\MCM-ICM2018\\data\\csv\\seseds.csv', skiprows=None, engine='c', low_memory=True)
-msncodes = pd.read_csv('C:\\Users\\THINKPAD\\PycharmProjects\\MCM-ICM2018\\data\\csv\\msncodes.csv', skiprows=None, engine='c', low_memory=True)
+parent_path = os.path.realpath(os.pardir)
+if sys.platform.startswith('win') or sys.platform.startswith('cygwin'):
+    seseds_path = os.path.join(parent_path, 'MCM-ICM-2018-Problem-C\\data\\csv\\seseds.csv')
+    msncodes_path = os.path.join(parent_path, 'MCM-ICM-2018-Problem-C\\data\\csv\\msncodes.csv')
+elif sys.platform.startswith('darwin') or sys.platform.startswith('linux'):
+    seseds_path = os.path.join(parent_path, 'MCM-ICM-2018-Problem-C/data/csv/seseds.csv')
+    msncodes_path = os.path.join(parent_path, 'MCM-ICM-2018-Problem-C/data/csv/msncodes.csv')
+else:
+    pass
+seseds = pd.read_csv(seseds_path, skiprows=None, engine='c', low_memory=True)
+msncodes = pd.read_csv(msncodes_path, skiprows=None, engine='c', low_memory=True)
 # print(seseds) 
 # print(msncodes)
 # print(type(msncodes)) # dict
 # for key in msncodes.keys():
 #     print(msncodes[key])
 
-msn = []
-description = []
-unit = []
-for i in range(len(msncodes["Description"])):
-    if not re.search("price", msncodes["Description"][i]) and not re.search("expanditures", msncodes["Description"][i])\
-            and not re.search("production", msncodes["Description"][i]):
-        msn.append(msncodes["MSN"][i])
-        description.append(msncodes["Description"][i])
-        unit.append(msncodes["Unit"][i])
-data_frame = pd.DataFrame({"MSN": msn, "Description": description, "Unit": unit})
-data_frame.to_csv("C:\\Users\\THINKPAD\\PycharmProjects\\MCM-ICM2018\\data\\test.csv",index=False,index_label=False,sep=',')
+# msn = []
+# description = []
+# unit = []
+# for i in range(len(msncodes["Description"])):
+#     if not re.search("price", msncodes["Description"][i]) and not re.search("expanditures", msncodes["Description"][i])\
+#             and not re.search("production", msncodes["Description"][i]):
+#         msn.append(msncodes["MSN"][i])
+#         description.append(msncodes["Description"][i])
+#         unit.append(msncodes["Unit"][i])
+# data_frame = pd.DataFrame({"MSN": msn, "Description": description, "Unit": unit})
+# data_frame.to_csv("C:\\Users\\THINKPAD\\PycharmProjects\\MCM-ICM2018\\data\\test.csv",index=False,index_label=False,sep=',')
 
 
 
