e30a02cf7fbcacdbd3703ab0dd8fcfcbb065390a
Yulian Mao <mintmao@outlook.com>
2018-02-09 20:59:28 +0800

    some bugs

--- code/read.py
+++ code/read.py
@@ -4,18 +4,27 @@ import pandas as pd
 import re 
 import numpy as np
 
-seseds = pd.read_csv('data/csv/seseds.csv', skiprows=None, engine='c', low_memory=True)
-msncodes = pd.read_csv('data/csv/msncodes.csv', skiprows=None, engine='c', low_memory=True)
+seseds = pd.read_csv(b'data/csv/seseds.csv', skiprows=None, engine='c', low_memory=True)
+msncodes = pd.read_csv(b'data/csv/msncodes.csv', skiprows=None, engine='c', low_memory=True)
 # print(seseds) 
 # print(msncodes)
 # print(type(msncodes)) # dict
 # for key in msncodes.keys():
 #     print(msncodes[key])
 
+msn = []
+description = []
+unit = []
 for i in range(len(msncodes["Description"])):
-    if re.search("price", msncodes["Description"][i]) or re.search("expanditures", msncodes["Description"][i]) or re.search("production", msncodes["Description"][i]):
-        msncodes["Description"].drop(labels=i, axis=0)
-        # print(i)
-print(msncodes["Description"])
+    if not re.search("price", msncodes["Description"][i]) and not re.search("expanditures", msncodes["Description"][i])\
+            and not re.search("production", msncodes["Description"][i]):
+        msn.append(msncodes["MSN"][i])
+        description.append(msncodes["Description"][i])
+        unit.append(msncodes["Unit"][i])
+data_frame = pd.DataFrame({"MSN": msn, "Description": description, "Unit": unit})
+data_frame.to_csv("data/test.csv",index=False,index_label=False,sep=',')
+
+
+
 
 
