c2335150235861dfb9939581341ba56442f9e562
Yulian Mao <mintmao@outlook.com>
2018-02-12 01:40:29 +0800

    predict and some change of the program

--- code/PCR/huigui.py
+++ code/PCR/huigui.py
@@ -4,8 +4,8 @@ import numpy as np
 import os
 import sys
 from collections import OrderedDict
-
-
+import copy
+from sklearn.linear_model import LinearRegression
 
 # parent_path = os.path.realpath(os.pardir)
 # if sys.platform.startswith('win') or sys.platform.startswith('cygwin'):
@@ -16,7 +16,9 @@ from collections import OrderedDict
 #     msncodes_path = os.path.join(parent_path, 'MCM-ICM-2018-Problem-C/data/csv/msncodes.csv')
 # else:
 #     pass
-data = pd.read_csv("C:\\Users\\THINKPAD\\PycharmProjects\\MCM-ICM-2018-Problem-C\\code\\PCR\\ca_data.csv",
+data_test = pd.read_csv("C:\\Users\\THINKPAD\\PycharmProjects\\MCM-ICM-2018-Problem-C\\code\\PCR\\az_result_test.csv",
+                   skiprows=None, engine='c', low_memory=True)
+data_train = pd.read_csv("C:\\Users\\THINKPAD\\PycharmProjects\\MCM-ICM-2018-Problem-C\\code\\PCR\\az_result_train.csv",
                    skiprows=None, engine='c', low_memory=True)
 variable_data = pd.read_csv("C:\\Users\\THINKPAD\\PycharmProjects\\MCM-ICM-2018-Problem-C\\code\PCR\\variables.csv",
                        skiprows=None, engine='c', low_memory=True)
@@ -25,28 +27,16 @@ variable = []
 for i in variable1:
     if i not in variable:
         variable.append(i)
-ca_data = OrderedDict()
-for i in range(0, len(variable)):
-    ca_data[variable[i]] = np.zeros(50)
-for i in range(0, 50):
-    for j in range(len(data)):
-        if int(data["Year"][j])-1960 == i:
-            if data["MSN"][j] in variable:
-                ca_data[data["MSN"][j]][i] = data["Data"][j]
-year = []
-for i in range(1960, 2010):
-    year.append(i)
-ca_data["Year"] = year
-for i in variable:
-    if i != "TEGDS" and i != "Year":
-        mean = np.mean(ca_data[i])
-        std = np.std(ca_data[i])
-        if std != 0:
-            for j in range(len(ca_data[i])):
-                ca_data[i][j] = (ca_data[i][j]-mean)/std
-ca_comp_data = OrderedDict()
-ca_comp_data = pd.DataFrame(ca_data)
-ca_comp_data.to_csv("ca_data_by_year.csv", index=False, index_label=False, sep=',')
+feature_cols = ["CLTCV", "CLCCV", "CLACB", "ESCCV",	"ESRCV","ESTCB","ESTCV","GETCB","GERCB","LOTCB","NGCCB","NGICB",
+                "NGRCB","NNACB"]
+
+X = data_train[feature_cols]
+y = data_train["TEGDS"]
+linreg = LinearRegression()
+model = linreg.fit(X, y)
+print(model)
+print(linreg.intercept_)
+print(linreg.coef_)
 
 
 
