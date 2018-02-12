25501a15a9c4a9f51bfcf2de3706da095c7dd74b
spacebody <510662916@qq.com>
2018-02-12 23:12:41 +0800

    Update

--- code/part_I_C/plot_renewable.py
+++ code/part_I_C/plot_renewable.py
@@ -15,106 +15,145 @@ from renewable import az_reprb, az_retcb, az_roprb, az_tpopp, az_tetcb, az_teprb
 #plot
 
 #reprb and roprb
-sns.set_style("darkgrid")
-fig = plt.figure(figsize=(7, 5))
-ind = np.arange(4)    # the x locations for the groups
-width = 0.3
+# sns.set_style("darkgrid")
+# fig = plt.figure(figsize=(7, 5))
+# ind = np.arange(4)    # the x locations for the groups
+# width = 0.3
 
-p1 = plt.bar(ind, (az_reprb, ca_reprb, nm_reprb, tx_reprb), width)
-p2 = plt.bar(ind+width, (az_roprb, ca_roprb, nm_roprb, tx_roprb), width)
+# p1 = plt.bar(ind, (az_reprb, ca_reprb, nm_reprb, tx_reprb), width)
+# p2 = plt.bar(ind+width, (az_roprb, ca_roprb, nm_roprb, tx_roprb), width)
 
-plt.title("Renewable Energy Production of Four States")
-plt.xlabel("States")
-plt.ylabel("Data / Billion Btu")
-plt.xticks(ind+0.5*width, ('Arizona', 'California', 'New Mexico', 'Taxes'))
-plt.legend((p1[0], p2[0]), ('Include fuel ethanol', 'Exclude fuel ethanol'), loc='best')
-plt.savefig("code/part_I_C/figures/production.png")
-plt.savefig("code/part_I_C/figures/production.pdf")
-# plt.show()
+# plt.title("Renewable Energy Production of Four States")
+# plt.xlabel("States")
+# plt.ylabel("Data / Billion Btu")
+# plt.xticks(ind+0.5*width, ('Arizona', 'California', 'New Mexico', 'Taxes'))
+# plt.legend((p1[0], p2[0]), ('Include fuel ethanol', 'Exclude fuel ethanol'), loc='best')
+# plt.savefig("code/part_I_C/figures/production.png")
+# plt.savefig("code/part_I_C/figures/production.pdf")
+# # plt.show()
 
-#retcb
-sns.set_style("darkgrid")
-fig = plt.figure(figsize=(7, 5))
-ind = np.arange(4)    # the x locations for the groups
-width = 0.4
+# #retcb
+# sns.set_style("darkgrid")
+# fig = plt.figure(figsize=(7, 5))
+# ind = np.arange(4)    # the x locations for the groups
+# width = 0.4
 
-p1 = plt.bar(ind, (az_retcb, ca_retcb, nm_retcb, tx_retcb), width)
+# p1 = plt.bar(ind, (az_retcb, ca_retcb, nm_retcb, tx_retcb), width)
 
-plt.title("Renewable Energy Consumption of Four States")
-plt.xlabel("States")
-plt.ylabel("Data / Billion Btu")
-plt.xticks(ind, ('Arizona', 'California', 'New Mexico', 'Taxes'))
-plt.savefig("code/part_I_C/figures/consumption.png")
-plt.savefig("code/part_I_C/figures/consumption.pdf")
+# plt.title("Renewable Energy Consumption of Four States")
+# plt.xlabel("States")
+# plt.ylabel("Data / Billion Btu")
+# plt.xticks(ind, ('Arizona', 'California', 'New Mexico', 'Taxes'))
+# plt.savefig("code/part_I_C/figures/consumption.png")
+# plt.savefig("code/part_I_C/figures/consumption.pdf")
+
+
+t_az = 0.0
+t_ca = 0.0
+t_nm = 0.0
+t_tx = 0.0
 
 #retcb/tpopp
 sns.set_style("darkgrid")
-fig = plt.figure(figsize=(7, 5))
+fig = plt.figure(figsize=(14, 10))
+colors = ['#624ea7', 'g', 'r', 'b']
+plt.subplot(2, 2, 1)
 ind = np.arange(4)    # the x locations for the groups
 width = 0.4
+max_value = ca_retcb/ca_tpopp
+p1 = plt.bar(ind, (az_retcb/az_tpopp/max_value, ca_retcb/ca_tpopp/max_value,
+                   nm_retcb/nm_tpopp/max_value, tx_retcb/tx_tpopp/max_value), width, color=colors)
 
-p1 = plt.bar(ind, (az_retcb/az_tpopp, ca_retcb/ca_tpopp, nm_retcb/nm_tpopp, tx_retcb/tx_tpopp), width)
+t_az += az_retcb/az_tpopp/max_value
+t_ca += ca_retcb/ca_tpopp/max_value
+t_nm += nm_retcb/nm_tpopp/max_value
+t_tx += tx_retcb/tx_tpopp/max_value
 
 plt.title("Renewable Energy Consumption Per Capita of Four States")
 plt.xlabel("States")
-plt.ylabel("Data / Billion Btu/Thousand")
+plt.ylabel("Data / Score")
 plt.xticks(ind, ('Arizona', 'California', 'New Mexico', 'Taxes'))
-plt.savefig("code/part_I_C/figures/consumption_pop.png")
-plt.savefig("code/part_I_C/figures/consumption_pop.pdf")
+# plt.savefig("code/part_I_C/figures/consumption_pop.png")
+# plt.savefig("code/part_I_C/figures/consumption_pop.pdf")
 
 #reprb/tpopp
-sns.set_style("darkgrid")
-fig = plt.figure(figsize=(7, 5))
-ind = np.arange(4)    # the x locations for the groups
-width = 0.4
+# sns.set_style("darkgrid")
+# fig = plt.figure(figsize=(7, 5))
+plt.subplot(2, 2, 2)
+# ind = np.arange(4)    # the x locations for the groups
+# width = 0.4
+max_value = ca_reprb/ca_tpopp
+p2 = plt.bar(ind, (az_reprb/az_tpopp/max_value, ca_reprb/ca_tpopp/max_value,
+                   nm_reprb/nm_tpopp/max_value, tx_reprb/tx_tpopp/max_value), width, color=colors)
 
-p1 = plt.bar(ind, (az_reprb/az_tpopp, ca_reprb/ca_tpopp, nm_reprb/nm_tpopp, tx_reprb/tx_tpopp), width)
+t_az += az_reprb/az_tpopp/max_value
+t_ca += ca_reprb/ca_tpopp/max_value
+t_nm += nm_reprb/nm_tpopp/max_value
+t_tx += tx_reprb/tx_tpopp/max_value
 
 plt.title("Renewable Energy Production Per Captita of Four States")
 plt.xlabel("States")
-plt.ylabel("Data / Billion Btu/Thousand")
+plt.ylabel("Data / Score")
 plt.xticks(ind, ('Arizona', 'California', 'New Mexico', 'Taxes'))
-plt.savefig("code/part_I_C/figures/production_pop.png")
-plt.savefig("code/part_I_C/figures/production_pop.pdf")
+# plt.savefig("code/part_I_C/figures/production_pop.png")
+# plt.savefig("code/part_I_C/figures/production_pop.pdf")
 # plt.show()
 
 # retcb/tetcb
-sns.set_style("darkgrid")
-fig = plt.figure(figsize=(7, 5))
-ind = np.arange(4)    # the x locations for the groups
-width = 0.4
-
+# sns.set_style("darkgrid")
+# fig = plt.figure(figsize=(7, 5))
+plt.subplot(2, 2, 3)
+# ind = np.arange(4)    # the x locations for the groups
+# width = 0.4
+max_value = ca_retcb/ca_tetcb*100
 # p1 = plt.bar(ind, (100, 100, 100, 100), width)
-p2 = plt.bar(ind, (az_retcb/az_tetcb*100, ca_retcb/ca_tetcb*100,
-                   nm_retcb/nm_tetcb*100, tx_retcb/tx_tetcb*100), width)
+p3 = plt.bar(ind, (az_retcb/az_tetcb*100/max_value, ca_retcb/ca_tetcb*100/max_value,
+                   nm_retcb/nm_tetcb*100/max_value, tx_retcb/tx_tetcb*100/max_value), width, color=colors)
 
+t_az += az_retcb/az_tetcb*100/max_value
+t_ca += ca_retcb/ca_tetcb*100/max_value
+t_nm += nm_retcb/nm_tetcb*100/max_value
+t_tx += tx_retcb/tx_tetcb*100/max_value
 
 plt.title("Renewable Energy Consumption Percentage of Four States")
 plt.xlabel("States")
-plt.ylabel("Data / Percent(%)")
+plt.ylabel("Data / Score")
 plt.xticks(ind, ('Arizona', 'California', 'New Mexico', 'Taxes'))
-plt.savefig("code/part_I_C/figures/consumption_per.png")
-plt.savefig("code/part_I_C/figures/consumption_per.pdf")
+# plt.savefig("code/part_I_C/figures/consumption_per.png")
+# plt.savefig("code/part_I_C/figures/consumption_per.pdf")
 # plt.show()
 
 
 # reprb/teprb
-sns.set_style("darkgrid")
-fig = plt.figure(figsize=(7, 5))
-ind = np.arange(4)    # the x locations for the groups
-width = 0.4
+# sns.set_style("darkgrid")
+# fig = plt.figure(figsize=(7, 5))
+plt.subplot(2, 2, 4)
+# ind = np.arange(4)    # the x locations for the groups
+# width = 0.4
 
 # p1 = plt.bar(ind, (100, 100, 100, 100), width)
-p2 = plt.bar(ind, (az_reprb/az_teprb*100, ca_reprb/ca_teprb*100, nm_reprb/nm_teprb*100, tx_reprb/tx_teprb*100), width)
+max_value = ca_reprb/ca_teprb*100
+p4 = plt.bar(ind, (az_reprb/az_teprb*100/max_value, ca_reprb/ca_teprb*100/max_value,
+                   nm_reprb/nm_teprb*100/max_value, tx_reprb/tx_teprb*100/max_value), width, color=colors)
 
+t_az += az_reprb/az_teprb*100/max_value
+t_ca += ca_reprb/ca_teprb*100/max_value
+t_nm += nm_reprb/nm_teprb*100/max_value
+t_tx += tx_reprb/tx_teprb*100/max_value
 
 plt.title("Renewable Energy Production Percentage of Four States")
 plt.xlabel("States")
-plt.ylabel("Data / Percent(%)")
+plt.ylabel("Data / Score")
 plt.xticks(ind, ('Arizona', 'California', 'New Mexico', 'Taxes'))
-plt.savefig("code/part_I_C/figures/production_per.png")
-plt.savefig("code/part_I_C/figures/production_per.pdf")
+plt.savefig("code/part_I_C/figures/renewable_estimation.png")
+plt.savefig("code/part_I_C/figures/renewable_estimation.pdf")
+
+plt.legend([p1, p2, p3, p4], [
+           ('Arizona', 'California', 'New Mexico', 'Taxes')])
 # plt.show()
+print(t_az, t_ca, t_nm, t_tx)
+
+
 
 
 
--- code/part_I_D/state_complete_data.py
+++ code/part_I_D/state_complete_data.py
@@ -8,72 +8,72 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-complete = pd.read_csv("data/csv/original/complete.csv", engine='c', low_memory=True)
+# complete = pd.read_csv("data/csv/original/complete.csv", engine='c', low_memory=True)
 
-az_complete = OrderedDict()
-ca_complete = OrderedDict()
-nm_complete = OrderedDict()
-tx_complete = OrderedDict()
+# az_complete = OrderedDict()
+# ca_complete = OrderedDict()
+# nm_complete = OrderedDict()
+# tx_complete = OrderedDict()
 
-az_msn = []
-az_year = []
-az_data = []
+# az_msn = []
+# az_year = []
+# az_data = []
 
-ca_msn = []
-ca_year = []
-ca_data = []
+# ca_msn = []
+# ca_year = []
+# ca_data = []
 
-nm_msn = []
-nm_year = []
-nm_data = []
+# nm_msn = []
+# nm_year = []
+# nm_data = []
 
-tx_msn = []
-tx_year = []
-tx_data = []
+# tx_msn = []
+# tx_year = []
+# tx_data = []
 
-for i in range(len(complete["MSN"])):
-    if complete["StateCode"][i] == "AZ":
-        az_msn.append(complete["MSN"][i])
-        az_year.append(complete["Year"][i])
-        az_data.append(complete["Data"][i])
-    elif complete["StateCode"][i] == "CA":
-        ca_msn.append(complete["MSN"][i])
-        ca_year.append(complete["Year"][i])
-        ca_data.append(complete["Data"][i])
-    elif complete["StateCode"][i] == "NM":
-        nm_msn.append(complete["MSN"][i])
-        nm_year.append(complete["Year"][i])
-        nm_data.append(complete["Data"][i])
-    elif complete["StateCode"][i] == "TX":
-        tx_msn.append(complete["MSN"][i])
-        tx_year.append(complete["Year"][i])
-        tx_data.append(complete["Data"][i])
-    else:
-        pass
+# for i in range(len(complete["MSN"])):
+#     if complete["StateCode"][i] == "AZ":
+#         az_msn.append(complete["MSN"][i])
+#         az_year.append(complete["Year"][i])
+#         az_data.append(complete["Data"][i])
+#     elif complete["StateCode"][i] == "CA":
+#         ca_msn.append(complete["MSN"][i])
+#         ca_year.append(complete["Year"][i])
+#         ca_data.append(complete["Data"][i])
+#     elif complete["StateCode"][i] == "NM":
+#         nm_msn.append(complete["MSN"][i])
+#         nm_year.append(complete["Year"][i])
+#         nm_data.append(complete["Data"][i])
+#     elif complete["StateCode"][i] == "TX":
+#         tx_msn.append(complete["MSN"][i])
+#         tx_year.append(complete["Year"][i])
+#         tx_data.append(complete["Data"][i])
+#     else:
+#         pass
 
-az_complete["MSN"] = az_msn
-az_complete["Year"] = az_year
-az_complete["Data"] = az_data
-az_complete_data = pd.DataFrame(az_complete)
-az_complete_data.to_csv("data/csv/state_data/az_complete.csv", index=False, index_label=False, sep=',')
+# az_complete["MSN"] = az_msn
+# az_complete["Year"] = az_year
+# az_complete["Data"] = az_data
+# az_complete_data = pd.DataFrame(az_complete)
+# az_complete_data.to_csv("data/csv/state_data/az_complete.csv", index=False, index_label=False, sep=',')
 
-ca_complete["MSN"] = ca_msn
-ca_complete["Year"] = ca_year
-ca_complete["Data"] = ca_data
-ca_complete_data = pd.DataFrame(ca_complete)
-ca_complete_data.to_csv("data/csv/state_data/ca_complete.csv", index=False, index_label=False, sep=',')
+# ca_complete["MSN"] = ca_msn
+# ca_complete["Year"] = ca_year
+# ca_complete["Data"] = ca_data
+# ca_complete_data = pd.DataFrame(ca_complete)
+# ca_complete_data.to_csv("data/csv/state_data/ca_complete.csv", index=False, index_label=False, sep=',')
 
-nm_complete["MSN"] = nm_msn
-nm_complete["Year"] = nm_year
-nm_complete["Data"] = nm_data
-nm_complete_data = pd.DataFrame(nm_complete)
-nm_complete_data.to_csv("data/csv/state_data/nm_complete.csv", index=False, index_label=False, sep=',')
+# nm_complete["MSN"] = nm_msn
+# nm_complete["Year"] = nm_year
+# nm_complete["Data"] = nm_data
+# nm_complete_data = pd.DataFrame(nm_complete)
+# nm_complete_data.to_csv("data/csv/state_data/nm_complete.csv", index=False, index_label=False, sep=',')
 
-tx_complete["MSN"] = tx_msn
-tx_complete["Year"] = tx_year
-tx_complete["Data"] = tx_data
-tx_complete_data = pd.DataFrame(tx_complete)
-tx_complete_data.to_csv("data/csv/state_data/tx_complete.csv", index=False, index_label=False, sep=',')
+# tx_complete["MSN"] = tx_msn
+# tx_complete["Year"] = tx_year
+# tx_complete["Data"] = tx_data
+# tx_complete_data = pd.DataFrame(tx_complete)
+# tx_complete_data.to_csv("data/csv/state_data/tx_complete.csv", index=False, index_label=False, sep=',')
 
 az_new = OrderedDict()
 ca_new = OrderedDict()
@@ -102,7 +102,7 @@ nm_data = pd.read_csv("data/csv/state_data/nm_complete.csv", engine='c', low_mem
 tx_data = pd.read_csv("data/csv/state_data/tx_complete.csv", engine='c', low_memory=True)
 
 for i in range(len(az_data["MSN"])):
-    if az_data["Year"][i] > 2009:
+    if az_data["Year"][i] > 2009 and az_data["MSN"][i] == "TETCB":
         az_new_msn.append(az_data["MSN"][i])
         az_new_year.append(az_data["Year"][i])
         az_new_data.append(az_data["Data"][i])
@@ -117,7 +117,7 @@ az_new.to_csv("data/csv/state_data/az_new_data.csv", index=False, index_label=Fa
 
 
 for i in range(len(ca_data["MSN"])):
-    if ca_data["Year"][i] > 2009:
+    if ca_data["Year"][i] > 2009 and ca_data["MSN"][i]== "TETCB":
         ca_new_msn.append(ca_data["MSN"][i])
         ca_new_year.append(ca_data["Year"][i])
         ca_new_data.append(ca_data["Data"][i])
@@ -132,7 +132,7 @@ ca_new.to_csv("data/csv/state_data/ca_new_data.csv", index=False, index_label=Fa
 
 
 for i in range(len(nm_data["MSN"])):
-    if nm_data["Year"][i] > 2009:
+    if nm_data["Year"][i] > 2009 and nm_data["MSN"][i] == "TETCB":
         nm_new_msn.append(nm_data["MSN"][i])
         
         nm_new_year.append(nm_data["Year"][i])
@@ -148,7 +148,7 @@ nm_new = pd.DataFrame(nm_new)
 nm_new.to_csv("data/csv/state_data/nm_new_data.csv", index=False, index_label=False, sep=',')
 
 for i in range(len(tx_data["MSN"])):
-    if tx_data["Year"][i] > 2009:
+    if tx_data["Year"][i] > 2009 and tx_data["MSN"][i] == "TETCB":
         tx_new_msn.append(tx_data["MSN"][i])
         tx_new_year.append(tx_data["Year"][i])
         tx_new_data.append(tx_data["Data"][i])
